c36986aaeac5f26e5a3fa6557d0546bb017bb669
pilimuino <pili.muino@gmail.com>
2024-09-06 13:14:36 +0200

    [ST-20]feat: ST-21 Develop HomeComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Navbar from '@/components/navbar/Navbar';
 import './index.css';
 import Footer from './components/footer/Footer';
-import Button from './components/button/Button';
+import Home from './components/home/HomeComponent'
 
 function App() {
   return (
-    <div >
+    <div className="flex flex-col min-h-screen">
       <Navbar/>
       <main>
-      <Button/>
+        <Home/>
       </main>
       <Footer/>
     </div>
