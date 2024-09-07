b364d9405d3a85151dbfe927774735ceb3cea3a8
pilimuino <pili.muino@gmail.com>
2024-09-07 19:17:03 +0200

    [ST-36]feat: Terms of use, privacy, cookies added

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -4,8 +4,19 @@ import React from 'react'
 const Privacy = () => {
     return (
         <div>
-            <h1>Privacy</h1>
-        </div>
+            <h1 className="text-xl font-bold md:mt-10 text-[#a6a6a6]">PRIVACY</h1>
+            <h2 className='text-l font-semibold md:mt-3 text-[#a6a6a6]'>PRIVACY POLICIES:</h2>
+            <div className="mb-6 md:mb-2 md:w md:mt-2  md:pr-13">
+                <p className='text-l text-[#a6a6a6]'>Studentest respects current legislation on personal data protection, user privacy and the confidentiality and security of personal data, in accordance with the provisions of the new General Data Protection Regulation (EU Regulation 2016/679), adopting the necessary technical and organizational measures to prevent the loss, misuse, alteration, unauthorized access and theft of the personal data provided, taking into account the state of technology, the nature of the data and the risks to which they are exposed.
+                By registering a user account, the user gives his or her consent to Studentest to process his or her personal data in accordance with Spanish legislation. All the information we collect is related to providing our services.
+                Studentest will update this Data Protection Policy occasionally and in response to requirements arising from legal, regulatory or operational changes. 
+                </p>
+                <p className='text-l md:mt-2 text-[#a6a6a6]'>
+                In that case, the user will be notified of such changes (including their entry into force) in accordance with the law. Your continued use of the Studentest service after such updates become effective will constitute acceptance of such changes
+                (Both Studentest and its service providers use cookies and other technologies (such as web beacons) for a variety of reasons such as to facilitate your access to our services by reminding you when you return, to provide and analyse our services, and to provide and personalise marketing and advertising services.)
+                You may at any time exercise your rights of access, rectification, cancellation and opposition by using the contact form available on this website or by sending an email to: student@example.com, stating the reason for your request.
+                </p>
+        </div></div>
     )
 }
 
--- src/pages/TermsOfUse.jsx
+++ src/pages/TermsOfUse.jsx
@@ -4,7 +4,31 @@ import React from 'react'
 const TermsOfUse = () => {
     return (
         <div>
-            <h1>Terms</h1>
+            <h1 className="text-xl font-bold md:mt-10 text-[#a6a6a6]">OUR TERMS AND CONDITIONS</h1>
+                <h2 className="text-xl font-bold md:mt-3 text-[#a6a6a6]">THANK YOU FOR CHOOSING Studentest</h2>
+                    <p className='text-l md:mt-2 text-[#a6a6a6]'>
+                        This document, together with our Data Protection Policy and Cookies Policy, establishes the conditions governing the use of this website (www.Student.com), access to its contents and the contracting of services through it. Please read these Conditions, our Data Protection Policy and the Cookies Policy carefully before using this website. By using this website, contracting a service through it or accessing any content or material that Studentest publishes, you agree to be bound by these Conditions, our Data Protection Policy and the Cookies Policy, so if you do not agree with everything established in them, you should not use this website.
+                        If you have any questions related to these Conditions, our Data Protection Policy or our Cookies Policy, you can contact us through the contact form available on our website.
+                    </p>
+                    <h2 className="text-xl font-bold md:mt-3 text-[#a6a6a6]">OUR DATA USER DATA</h2>
+                    <p className='text-l md:mt-2 text-[#a6a6a6]'>
+                        The information or personal data that you provide us about yourself will be treated in accordance with the provisions of our Data Protection Policy. By using this website, you consent to the processing of said information and data and declare that all the information or data that you provide us is true and corresponds to reality. If you detect that an error has occurred when entering your personal data during your registration as a user of this website, you may modify them by contacting the customer service department through the contact form available on this website or by email to student@example.com, as well as exercising the right of rectification contemplated in our Data Protection Policy.
+                    </p>
+                <h2 className="text-xl font-bold md:mt-3 text-[#a6a6a6]">REGISTRATION, PASSWORD AND ACCOUNT ACCESS</h2>
+                    <p className='text-l md:mt-2 text-[#a6a6a6]'>
+                    During the registration process, the user will provide a username and password. The login information is personal for the exclusive use of the account holder and may not be used by a third party. The user will store the login information in order to protect it from unauthorized access. The user is responsible for all use of the service that occurs through his/her account. If the user suspects any unauthorized use of the login information, he/she must immediately notify Studentest and change his/her password. In the event that Studentest has reason to believe that the login information has been disclosed or is otherwise being misused by an unauthorized person, Studentest shall have the right to immediately cancel the agreement and suspend the service, without refunding any amounts paid and not accrued, as a penalty. Studentest may cancel an account or block it if it detects that the password has been shared or that there has been a case of identity theft or other fraudulent activity.
+                    </p>
+                <h2 className="text-xl font-bold md:mt-3 text-[#a6a6a6]">USE OF THE SERVICE</h2>
+                    <p className='text-l md:mt-2 text-[#a6a6a6]'>
+                        In order to use our services, the user must register a user account on the Studentest website, have access to a computer with an Internet connection and compatible with Studentest, and provide us with an accepted, valid and current payment method in accordance with these Terms and Conditions.
+                        After making the payment, the user will receive an email acknowledging receipt of their order and with instructions on how to begin enjoying the contracted service. Their access to the Studentest services will have a limited duration according to the contracted plan.
+                        The user may not carry out or incite, facilitate or cause others to carry out any of the following actions: use the Studentest services for a commercial or public purpose, for an illegal or inappropriate purpose; copy/reproduce, lend, retransmit, distribute, edit or transmit in any other way or adapt content offered by Studentest; circumvent, modify, remove, alter or otherwise manipulate any protection, encryption or other technology or software that is part of the Studentest service
+                        In the event that Studentest detects unauthorized use of the service or there is evidence to suggest that unauthorized use is occurring, this will be considered a material breach of the agreement that may result in Studentest canceling the service with immediate effect and the user will not be entitled to a refund of any fees paid to Studentest.
+                    </p>
+                <h2 className="text-xl font-bold md:mt-3 text-[#a6a6a6]">INDUSTRIAL AND INTELLECTUAL PROPERTY</h2>
+                    <p className='text-l f md:mt-2 md:mb-2 text-[#a6a6a6]'>
+                        The services and content offered by Studentest are the property of Studentest. The user acknowledges and agrees that all copyright, trademarks and other industrial and intellectual property rights on the materials or content provided as part of the website
+                    </p>
         </div>
     )
 }
