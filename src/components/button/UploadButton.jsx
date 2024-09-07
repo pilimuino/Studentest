54c06bc90e0f21e9dfcf5e868015fe190423b1ce
pilimuino <pili.muino@gmail.com>
2024-09-07 17:36:42 +0200

    [ST-29] feat: develop function upload

--- src/components/button/UploadButton.jsx
+++ src/components/button/UploadButton.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
 
+const UploadButton = ({ onClick }) => {
+    return (
+        <button
+            onClick={onClick}  
+            className="bg-white text-[#a6a6a6] border border-[#a6a6a6] font-semibold py-2 px-2 rounded-full shadow-lg hover:text-[#068ca4] transition duration-300"
+            style={{ width: '180px', height: '53px' }}
+        >
+            Upload document
+        </button>
+    );
+};
 
-const UploadButton = () => {
-        return (
-            <div>
-                <div className=" flex justify-end items-center z-10">
-                    <Link to="/uploadocument">
-                        <button className="bg-white text-[#a6a6a6] border border-[#a6a6a6] font-semibold py-2 px-2 rounded-full shadow-lg hover:text-[#068ca4] transition duration-300" style={{ width: '180px', height: '53px' }}>
-                            Upload document
-                        </button>
-                    </Link>
-                </div>    
-            </div>
-            );
-        }
+export default UploadButton;
 
-export default UploadButton
